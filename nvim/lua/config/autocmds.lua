f4412daafc0a84a5e0978883ff5929e6eef82bdb
Sushant Vema <sushant.h.vema@berkeley.edu>
2025-02-24 19:16:00 -0800

    as per CommonMark conventions, decided to adhere to 80 characters per line always

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.opt.formatoptions = "jcroqlnt"
+vim.opt.textwidth = 80
+
+-- autocmds
+
+vim.api.nvim_create_autocmd({ "BufWinEnter", "BufWinLeave" }, {
+  pattern = { "*.md" },
+  callback = function()
+    vim.opt.colorcolumn = "80"
+    vim.opt.textwidth = 80
+  end,
+})
