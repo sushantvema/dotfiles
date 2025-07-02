15a5885b73ff192368457605c2cdc65854d3adb7
Sushant H Vema <sashikanth@Sushants-MBP.local>
2025-07-02 11:54:24 -0700

    removed lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/plugins/lspconfig.lua
-return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      marksman = false, -- 🚫 disables default marksman setup
-    },
-  },
-}
