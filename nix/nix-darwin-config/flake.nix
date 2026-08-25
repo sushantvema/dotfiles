580f2dff19734fa4da35953dd62dbbbb42a58446
sushant <sushant@MacBookPro.lan>
2026-08-25 15:43:54 -0700

    chore: darwin rebuild first time

--- nix/nix-darwin-config/flake.nix
+++ nix/nix-darwin-config/flake.nix
@@ -31,6 +31,8 @@
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+      
+      security.pam.services.sudo_local.touchIdAuth = true;
     };
   in
   {
